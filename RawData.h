f009de6c42ffa84720fce74f35285f2754acfe54
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-01-26 16:09:47 +0000

    Create RawData

---
+++ RawData.h
@@ -0,0 +1,30 @@
+#ifndef __MAGNETICS_RAW_DATA_H__
+#define __MAGNETICS_RAW_DATA_H__
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+class RawData{
+    public:
+        RawData(uint8_t* data, uint8_t size) : data(data, data + size) {}
+
+        std::vector<uint8_t> to_vec(){
+            return data;
+        }
+
+        std::string to_string(){
+            std::string str(data.size(), '\0');
+
+            for( size_t i = 0; i < data.size(); ++i){
+                str[i] = (char)data[i];
+            }
+
+            return str;
+        }
+
+    private:
+        std::vector<uint8_t> data;
+};
+
+#endif
