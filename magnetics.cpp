0a03159e5292610e3e34a221cac1563e21a997d1
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-02-24 16:48:42 +0000

    Add scan functions

--- magnetics.cpp
+++ magnetics.cpp
@@ -6,16 +6,15 @@
 
 #include "BLEAdvertising.h"
 
-static BLEAdvertising *advertising = nullptr;
+BLEAdvertising *advertising = nullptr;
 
 using namespace pxt;
 
 namespace magnetics {
     
-
     void configure(){
         if( advertising == nullptr ){
-            advertising = new BLEAdvertising(*uBit.ble);
+            advertising = BLEAdvertising::getInstance(uBit.ble);
         }
     }
 
@@ -32,6 +31,18 @@ namespace magnetics {
     }
 
     //%
+    void startScanning(){
+        configure();
+        advertising->startScanning();
+    }
+
+    //%
+    void stopScanning(){
+        configure();
+        advertising->stopScanning();
+    }
+
+    //%
     void setData(String data){
         configure();
         advertising->setData(std::string(data->getUTF8Data()));
@@ -41,5 +52,45 @@ namespace magnetics {
     void setName(String name){
         configure();
         advertising->setDeviceName(std::string(name->getUTF8Data()));
+    }
+
+    //%
+    int available(){
+        configure();
+        return (int)advertising->scanReportAvailable();
+    }
+
+    //%
+    bool availableScanFor(String name){
+        configure();
+
+        std::string peerName = std::string(name->getUTF8Data());
+        return advertising->scanReportAvailableFor(peerName);
+    }
+
+    //%
+    String getDataFrom(String name){
+        configure();
+
+        std::string peerName = std::string(name->getUTF8Data());
+        std::string result = advertising->getServiceDataFrom(peerName);
+
+        return mkString(result.data(), result.size());
+    }
+
+    //%
+    String peekDataFrom(String name){
+        configure();
+
+        std::string peerName = std::string(name->getUTF8Data());
+        std::string result = advertising->peekServiceDataFrom(peerName);
+
+        return mkString(result.data(), result.size());
+    }
+
+    //%
+    void onNewMessage(Action action){
+        configure();
+        advertising->onNewMessage(action);
     }
 }
