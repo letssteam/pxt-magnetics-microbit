152ce64b75783e980db7552754651e039f4ab4e5
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-02-24 17:04:31 +0000

    Add scanning function + doc

--- BLEAdvertising.cpp
+++ BLEAdvertising.cpp
@@ -3,16 +3,39 @@
 #if CONFIG_ENABLED(DEVICE_BLE)
 
 #include <cstdint>
+#include <list>
 #include "ble_advdata.h"
 #include "ble_gap.h"
+#include "nrf_sdh.h"
 
+#include "AdvertisingReport.h"
+
+#define microbit_ble_OBSERVER_PRIO  2
+
+constexpr uint8_t MAX_NB_ADVERTISING_REPORT = 16;
 constexpr uint8_t FLAGS[1] = {0x06}; // Br Edr Not Supported, LE General Discovery Mode
 constexpr uint16_t SERVICEDATA_UUID = 0x181C;
 
+static uint8_t scanReportBuffer[BLE_GAP_SCAN_BUFFER_MIN] = {0x00};
+static ble_data_t scanReport = { scanReportBuffer, sizeof(scanReportBuffer) };
+
+static std::list<ble_gap_evt_adv_report_t> reportsBuffer; 
 static void magnetics_ble_evt_handler(ble_evt_t const * event, void * context);
 
-BLEAdvertising::BLEAdvertising(BLEDevice &ble) : ble(ble), isAdvertise(false), isScanning(false), dataLength(0) {
-    NRF_SDH_BLE_OBSERVER( microbit_magnetics_observer, microbit_ble_OBSERVER_PRIO, magnetics_ble_evt_handler, this);
+BLEAdvertising* BLEAdvertising::instance = nullptr;
+
+BLEAdvertising* BLEAdvertising::getInstance(BLEDevice* ble){
+
+    if( instance == nullptr && ble != nullptr ){
+        instance = new BLEAdvertising(ble);
+    }
+
+    return instance;
+}
+
+void BLEAdvertising::setData(std::string str){
+    dataLength = sprintf(data, "%s", str.substr(0, DATA_MAX_SIZE).c_str());
+    updateAdvertising();
 }
 
 void BLEAdvertising::startAdvertising()
@@ -23,7 +46,6 @@ void BLEAdvertising::startAdvertising()
 
     stopAdvertising();
     setGapName();
-
 
     uint8_t advHandle;
     static ble_advdata_t advData;
@@ -51,8 +73,6 @@ void BLEAdvertising::startAdvertising()
 
     advScanData.p_service_data_array = serviceData;
     advScanData.service_data_count = 1;
-
-
 
     ble_gap_adv_params_t    gap_adv_params;
     memset( &gap_adv_params, 0, sizeof( gap_adv_params));
@@ -83,13 +103,13 @@ void BLEAdvertising::startAdvertising()
     MICROBIT_BLE_ECHK( ble_advdata_encode( &advScanData, gap_adv_data.scan_rsp_data.p_data, &gap_adv_data.scan_rsp_data.len));
     MICROBIT_BLE_ECHK( sd_ble_gap_adv_set_configure( &advHandle, &gap_adv_data, &gap_adv_params));
 
-    ble.advertise();
+    ble->advertise();
     isAdvertise = true;
 }
 
 void BLEAdvertising::stopAdvertising()
 {
-    ble.stopAdvertising();
+    ble->stopAdvertising();
     isAdvertise = false;
 }
 
@@ -100,18 +120,143 @@ void BLEAdvertising::startScanning(){
         stopAdvertising();
     }
 
+    static ble_gap_scan_params_t const scanParam =
+    {
+        .extended                   = 0,
+        .report_incomplete_evts     = 0,
+        .active                     = 1,
+        .filter_policy              = BLE_GAP_SCAN_FP_ACCEPT_ALL,
+        .scan_phys                  = BLE_GAP_PHY_1MBPS,
+        .interval                   = 0x0050,
+        .window                     = 0x0050,
+        .timeout                    = 0x0000,
+        .channel_mask               = {0x00, 0x00, 0x00, 0x00, 0x00}
+    };
+
+    char code[11] = {0x00};
+
+    uint32_t res = sd_ble_gap_scan_start(&scanParam, &scanReport);
+    
+    if( res == NRF_SUCCESS){
+    }
+    else{
+        uBit.display.scroll("Scan Fail");
+        sprintf(code, "0x%0.8X", res);
+        uBit.display.scroll(code);
+    }
+
     isScanning = true;
 }
 
 void BLEAdvertising::stopScanning(){
-    MICROBIT_BLE_ECHK( sd_ble_gap_scan_stop(void) );
+    MICROBIT_BLE_ECHK( sd_ble_gap_scan_stop() );
     isScanning = false;
 }
 
-void BLEAdvertising::handleAdvertisingReport(ble_gap_evt_adv_report_t& report){
+void BLEAdvertising::handleAdvertisingReport(){
+    static bool isHandleRunning = false;
 
+    if( isHandleRunning ){ return; }
+
+    isHandleRunning = true;
+
+    while(reportsBuffer.size() > 0){
+        AdvertisingReport advRep(reportsBuffer.front());
+        reportsBuffer.pop_front();
+
+        bool isNew = true;
+
+        for( uint8_t i = 0; i < advReports.size(); ++i){
+            if( advReports[i].address == advRep.address ){
+                advReports[i].serviceData.uuid = advRep.serviceData.uuid;
+
+                if( advReports[i].name != advRep.name && !advRep.name.empty()){
+                    advReports[i].name = advRep.name;
+                    fireOnNewMessageCallbacks();
+                }
+
+                if( !advRep.serviceData.data.empty() && advRep.serviceData.uuid != 0x0000 && advReports[i].serviceData.data != advRep.serviceData.data ){
+                    advReports[i].serviceData.data = advRep.serviceData.data;
+                    fireOnNewMessageCallbacks();
+                }
+
+                isNew = false;
+                break;
+            }
+        }
+
+        if( isNew ){
+            if(advReports.size() >= MAX_NB_ADVERTISING_REPORT){
+                return;
+            }
+            else{
+                advReports.push_back(advRep);
+                fireOnNewMessageCallbacks();
+            }
+        }
+    }
+
+    isHandleRunning = false;
 }
 
+uint8_t BLEAdvertising::scanReportAvailable(){
+    return advReports.size();
+}
+
+
+bool BLEAdvertising::scanReportAvailableFor(std::string& name){
+
+    for(auto report : advReports){
+        if( report.name == name ){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+std::vector<AdvertisingReport> BLEAdvertising::getAllScanReport(){
+    auto results = std::vector<AdvertisingReport>(advReports);
+    advReports.clear();
+
+    return results;
+}
+
+std::string BLEAdvertising::getServiceDataFrom(std::string& name){
+    std::string result = "";
+
+    for( auto it = advReports.begin(); it != advReports.end(); ++it){
+        if( it->name == name ){
+            result = it->serviceData.data.to_string();
+            advReports.erase(it);
+            break;
+        }
+    }
+
+    return result;
+}
+
+std::string BLEAdvertising::peekServiceDataFrom(std::string& name){
+    std::string result = "";
+
+    for( auto it = advReports.begin(); it != advReports.end(); ++it){
+        if( it->name == name ){
+            result = it->serviceData.data.to_string();
+            break;
+        }
+    }
+
+    return result;
+}
+
+void BLEAdvertising::onNewMessage(pxt::Action action){
+    onNewMessageEvents.push_back(action);
+}
+
+BLEAdvertising::BLEAdvertising(BLEDevice *ble) : ble(ble), isAdvertise(false), isScanning(false), dataLength(0) {
+    stopAdvertising();
+    NRF_SDH_BLE_OBSERVER( microbit_magnetics_observer, microbit_ble_OBSERVER_PRIO, magnetics_ble_evt_handler, NULL);
+}
 
 void BLEAdvertising::updateAdvertising()
 {
@@ -135,24 +280,31 @@ void BLEAdvertising::setGapName()
     MICROBIT_BLE_ECHK(sd_ble_gap_device_name_set(&permissions, (uint8_t *)gapName, len));
 }
 
-void BLEAdvertising::setData(std::string str){
-    dataLength = sprintf(data, "%s", str.substr(0, DATA_MAX_SIZE).c_str());
-
-    updateAdvertising();
+void BLEAdvertising::fireOnNewMessageCallbacks(){
+    for( auto elem : onNewMessageEvents ){
+        pxt::runInParallel(elem);
+    }
 }
 
+static void magnetics_ble_evt_handler(ble_evt_t const * event, void * context){
 
-void magnetics_ble_evt_handler(ble_evt_t const * event, void * context){
+    auto inst = BLEAdvertising::getInstance();
 
-    switch (event->header.evt_id)
-    {
-        case BLE_GAP_EVT_ADV_REPORT:
-            ((BLEAdvertising*)context)->handleAdvertisingReport( event->gap_event.adv_report );
-            break;
-        
-        default:
-            break;
+    if( inst != nullptr ){
+        switch(event->header.evt_id){
+            case BLE_GAP_EVT_ADV_REPORT:
+                if( reportsBuffer.size() < MAX_NB_ADVERTISING_REPORT ){
+                    reportsBuffer.push_back(event->evt.gap_evt.params.adv_report);
+                    inst->handleAdvertisingReport();
+                }
+                break;
+
+            default:
+                break;
+        }
     }
+
+    sd_ble_gap_scan_start(NULL, &scanReport);
 }
 
 #endif
--- BLEAdvertising.h
+++ BLEAdvertising.h
@@ -12,31 +12,85 @@
 #include "ble_gap.h"
 #include "nrf_sdh_ble.h"
 #include <string>
+#include <vector>
+#include <functional>
 
+#include "AdvertisingReport.h"
 
 
 constexpr int NAME_MAX_SIZE = 10;
 constexpr int DATA_MAX_SIZE = 20;
 
-
 class BLEAdvertising
 {
 
     public:
-        BLEAdvertising(BLEDevice &ble);
+        /// @brief Get the instance of BLEAdvertising (created if needed and if ble parameter is provided)
+        /// @param ble BLEDevice instance
+        /// @return BLEAdvertising object if exist (or created) or nullptr if no instance exist (and no ble parameter)
+        static BLEAdvertising* getInstance(BLEDevice* ble = nullptr);
+
+
+        /// @brief Set the device name during advertising
+        /// @param name The name of the device
         void setDeviceName(std::string name) { this->name = name; updateAdvertising(); }
+
+
+        /// @brief Set the data sent with advertising packets
+        /// @param str The data
         void setData(std::string str);
 
+
+        /// @brief Start the advertisment (If the scan is in progress, it will be halted)
         void startAdvertising();
+
+        /// @brief Stop the advertisment
         void stopAdvertising();
 
+        /// @brief Start the scanning (If the advertisment is in progress, it will be halted)
         void startScanning();
+
+        /// @brief Stop the scanning
         void stopScanning();
 
-        void handleAdvertisingReport(ble_gap_evt_adv_report_t& report);
+        /// @brief Handle an advertising report (or scanning report)
+        /// @param report the report to handle
+        void handleAdvertisingReport();
+
+
+        /// @brief Get how many scan result is available
+        /// @return 
+        uint8_t scanReportAvailable();
+
+
+        /// @brief Get if any report is avaible for device with specific name
+        /// @param name The device name
+        /// @return 
+        bool scanReportAvailableFor(std::string& name);
+        
+        /// @brief Get all available report. WARNING: They will be removed from the available reports 
+        /// @return Vector of reports
+        std::vector<AdvertisingReport> getAllScanReport();
+        
+        /// @brief Get available report for specific name. WARNING: It will be removed from the available reports 
+        /// @param name device name
+        /// @return 
+        std::string getServiceDataFrom(std::string& name);
+        
+        /// @brief Get available report for specific name. WARNING: It will *NOT* be removed from the available reports 
+        /// @param name device name
+        /// @return 
+        std::string peekServiceDataFrom(std::string& name);
+
+        /// @brief Register a action, when new (and different) message arrived from device.
+        /// @param action 
+        void onNewMessage(pxt::Action action);
+
 
     private:
-        BLEDevice &ble;
+        static BLEAdvertising* instance;
+
+        BLEDevice *ble;
         bool isAdvertise;
         bool isScanning;
         std::string name;
@@ -45,8 +99,13 @@ class BLEAdvertising
         char data[DATA_MAX_SIZE];
         char gapName[28];
 
+        std::vector<AdvertisingReport> advReports;
+        std::vector<pxt::Action> onNewMessageEvents; 
+
+        BLEAdvertising(BLEDevice *ble);
         void updateAdvertising();
         void setGapName();
+        void fireOnNewMessageCallbacks();
 };
 
 #endif
